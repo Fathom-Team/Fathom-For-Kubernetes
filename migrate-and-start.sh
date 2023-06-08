1cccd3c759dcd54b8ef74f9c00ff070d3bab8d71
Melissa Armstrong <mkarmstr@gmail.com>
2023-06-08 14:15:51 -0400

    end

--- migrate-and-start.sh
+++ migrate-and-start.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-sleep 10
+echo "Waiting for postgres..."
+
+while ! nc -z postgres-db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
 npx prisma db push
 npx prisma migrate deploy
 pnpm start
